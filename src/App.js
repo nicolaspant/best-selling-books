2b4972b85d5481c02f83c60fc053443006e3f578
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-19 18:19:46 -0400

    added dummy button

--- src/App.js
+++ src/App.js
@@ -33,11 +33,17 @@ function Booklist() {
 }
 
 const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert('hello world')
+  }
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
+      <button type='button' onClick={clickHandler}>
+        Buy It!
+      </button>
     </article>
   )
 }
