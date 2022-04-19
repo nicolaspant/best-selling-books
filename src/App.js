556153e6c6305d6ed5a73b09e4377a3748270fc9
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-19 16:45:04 -0400

    replaced variables with props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import './app.css'
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg',
+  title: 'The Very Hungry Caterpillar',
+  author: 'Eric Carle',
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+}
 
 function Booklist() {
   return (
     <div className='book-list'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </div>
   )
 }
 
-const Book = () => {
-  const title = 'The Very Hungry Caterpillar'
-  const author = 'Eric Carle'
-  const image = ''
+const Book = (props) => {
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg'
-        alt=''
-      />
-
-      <h1>{title}</h1>
-
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author.toUpperCase()}</h4>
     </article>
   )
 }
