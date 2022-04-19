de2f2671a57294c6272d5e51fb31e2387073155f
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-19 15:54:51 -0400

    hard coded title, image and author components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function Booklist() {
-  return <div></div>
+  return (
+    <div>
+      <Book />
+    </div>
+  )
 }
 
-export default App
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg'
+    alt=''
+  />
+)
+
+const Title = () => <h1>The Very Hungry Caterpillar</h1>
+
+const Author = () => <h2>Eric Carle </h2>
+
+export default Booklist
