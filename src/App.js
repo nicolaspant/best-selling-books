a71f17c6dc6d489d94372cc801732e96789e912f
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-19 18:40:24 -0400

    removed clutter by creating components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import './app.css'
-
-const books = [
-  {
-    id: 1,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg',
-    title: 'The Very Hungry Caterpillar',
-    author: 'Eric Carle',
-  },
-
-  {
-    id: 2,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg',
-    title: 'I Love You to the Moon and Back',
-    author: 'Amelia Hepworth',
-  },
-  {
-    id: 3,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL302_SR302,200_.jpg',
-    title: 'If Animals Kissed Good Night',
-    author: 'Ann Whitford Paul ',
-  },
-]
-
+import books from './books'
+import Book from './Book'
 function Booklist() {
   return (
     <div className='book-list'>
@@ -29,22 +8,6 @@ function Booklist() {
         return <Book key={book.id} {...book}></Book>
       })}
     </div>
-  )
-}
-
-const Book = ({ img, title, author }) => {
-  const clickHandler = () => {
-    alert('hello world')
-  }
-  return (
-    <article className='book'>
-      <img src={img} alt='' />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      <button type='button' onClick={clickHandler}>
-        Buy It!
-      </button>
-    </article>
   )
 }
 
---
+++ src/books.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+  },
+
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL302_SR302,200_.jpg',
+    title: 'If Animals Kissed Good Night',
+    author: 'Ann Whitford Paul ',
+  },
+]
+
+export default books
