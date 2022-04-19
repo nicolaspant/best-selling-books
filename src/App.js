c09bbec5abd3ebdc38620cf0cc712cb20ef22cdd
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-19 17:34:07 -0400

    created list and refactored

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
 import './app.css'
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg',
-  title: 'The Very Hungry Caterpillar',
-  author: 'Eric Carle',
-}
 
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-}
+const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL604_SR604,400_.jpg',
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+  },
+
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL302_SR302,200_.jpg',
+    title: 'If Animals Kissed Good Night',
+    author: 'Ann Whitford Paul ',
+  },
+]
 
 function Booklist() {
   return (
     <div className='book-list'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ea
-          natus minus excepturi aperiam nam. Deleniti eos provident quidem
-          magni.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>
+      })}
     </div>
   )
 }
 
-const Book = ({ img, title, author, children }) => {
-  //destructuring the same as below
-  // const { img, title, author, children } = props
+const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
-      {children}
     </article>
   )
 }
